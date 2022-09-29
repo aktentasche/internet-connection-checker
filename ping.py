aec2090b9b68f2e19f5cea2bdeb17b2ce0cba998
Jonas <jonas@amps.one>
2022-09-29 22:26:58 +0200

    completed ping test script

--- ping.py
+++ ping.py
@@ -1,3 +1,22 @@
+import time
 from pythonping import ping
+import csv
 
-foo = ping("195.186.1.192", verbose=True)
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+
+parser.add_argument('--nameserver',
+                    choices=["swisscom", "google", "cloudflare"],
+                    action="store")
+
+args = parser.parse_args()
+
+nameservers = {"swisscom":"195.186.1.192", "google":"8.8.8.8", "cloudflare":"1.1.1.1"}
+
+while True:
+    ping_response = ping(nameservers[args.nameserver], verbose=True, size=1024, interval=0.5, count=20)
+
+    with open(f"ping_{args.nameserver}.csv", 'a') as f:
+        writer = csv.writer(f) #this is the writer object
+        writer.writerow([time.time(), ping_response.rtt_avg_ms, ping_response.rtt_min_ms, ping_response.rtt_max_ms, ping_response.packet_loss])
--- speedtest.py
+++ speedtest.py
@@ -1,15 +1,8 @@
 import speedtest
 from pythonping import ping
 
-foo = ping('127.0.0.1', verbose=True)
 
 servers = [3188] # iway.ch server
-# If you want to test against a specific server
-# servers = [1234]
-
-threads = None
-# If you want to use a single threaded test
-# threads = 1
 
 s = speedtest.Speedtest(secure=True)
 s.get_servers(servers)
@@ -19,4 +12,3 @@ s.upload()
 s.results.share()
 
 results_dict = s.results.dict()
-pass
